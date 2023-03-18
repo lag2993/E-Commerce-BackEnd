be336f5edd95bdee78c742f216eb924b38b8bf0a
Luis Guillen <lag2993@gmail.com>
2023-03-18 14:12:33 -0400

    fixed up some of the methods had some typo issues

--- models/index.js
+++ models/index.js
@@ -7,24 +7,22 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
   Product.belongsTo(Category,{
     foreignKey:'category_id',
-    
-  })
+    onDelete:'CASCADE',
+  });
 // Categories have many Products
   Category.hasMany(Product,{
     foreignKey:'category_id',
   })
 // Products belongToMany Tags (through ProductTag)
-Product.belongToMany(Tag,{
+Product.belongsToMany(Tag,{
   through: ProductTag,
   foreignKey:'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product,{
+Tag.belongsToMany(Product,{
   through: ProductTag,
   foreignKey:'tag_id',
 })
-
-
 module.exports = {
   Product,
   Category,
